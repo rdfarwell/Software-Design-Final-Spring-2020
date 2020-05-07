a5e7eb88c86d7a26580fb9094ab85ad4918bc7b9
nolpeterson <nolpeterson@uiowa.edu>
2020-05-07 00:38:51 -0500

    reformatted Client and Server

--- FantasySports/src/Client.java
+++ FantasySports/src/Client.java
@@ -11,44 +11,22 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Client extends JFrame implements Runnable {
-    private final String ID_1 = "1";
-    private final String ID_2 = "2";
     private final JTextArea displayArea;
-    private final JButton hit;
-    private final hitListener hitListener = new hitListener();
-    private final JButton stand;
-    private final standListener standListener = new standListener();
     private final JPanel panel;
-    private final JPanel subPanel;
     private Socket connection;
     private Scanner input;
     private Formatter output;
-    private final String BlackJackHost;
+    private final String Host;
     private String myID;
-    private boolean myTurn;
 
     public Client(String host) {
-        super("Blackjack");
-        BlackJackHost = host;
+        super("Fantasy");
+        Host = host;
         panel = new JPanel();
-        subPanel = new JPanel();
         displayArea = new JTextArea(14, 30);
         displayArea.setEditable(false);
-        //set background color of board to green
-        Color color = new Color(31, 170, 42);
-        displayArea.setBackground(color);
-        //set text color to white
-        displayArea.setForeground(Color.WHITE);
-        hit = new JButton("Hit");
-        hit.addActionListener(hitListener);
-        stand = new JButton("Stand");
-        stand.addActionListener(standListener);
         panel.setLayout(new BorderLayout());
-        subPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         panel.add(displayArea, BorderLayout.NORTH);
-        subPanel.add(hit);
-        subPanel.add(stand);
-        panel.add(subPanel, BorderLayout.SOUTH);
         add(panel);
         setSize(300, 300);
         setVisible(true);
@@ -60,7 +38,7 @@ public class Client extends JFrame implements Runnable {
         //connect to server and get streams
         try {
             //make connection to server - port range: 23503 - 23508
-            connection = new Socket(InetAddress.getByName(BlackJackHost), 23503);
+            connection = new Socket(InetAddress.getByName(Host), 23504);
 
             //get streams for input and output
             input = new Scanner(connection.getInputStream());
@@ -76,7 +54,6 @@ public class Client extends JFrame implements Runnable {
     }
 
     public void run() {
-        //get player's ID (1 or 2)
         myID = input.nextLine();
 
         SwingUtilities.invokeLater(
@@ -87,14 +64,6 @@ public class Client extends JFrame implements Runnable {
                     }
                 }
         );
-
-        //determine if client's turn
-        myTurn = (myID.equals(ID_1));
-        System.out.println((myID.equals(ID_1)));
-
-        //call dealHand to deal 2 cards to each player + 1 card to dealer
-        output.format("dealHand\n");
-        output.flush();
 
         //receive messages sent to client and output them
         while (true) {
@@ -110,15 +79,6 @@ public class Client extends JFrame implements Runnable {
             displayMessage("\nYou got a " + message);
             displayMessage("\nWould you like to Hit or Stand?");
         }
-        if (message.contains("output:"))
-        {
-            message = message.replace("output: ", "");
-            displayMessage("\n" + message);
-        }
-        if (message.contains("end:")) {
-            message = message.replace("end: ", "");
-            displayMessage("\n" + message);
-        }
     }
 
     private void displayMessage(final String messageToDisplay) {
@@ -129,34 +89,5 @@ public class Client extends JFrame implements Runnable {
                     }
                 }
         );
-    }
-
-    private class hitListener implements ActionListener {
-        /**
-         * Outputs a "hit\n" formatted message to server to make a "hit" on player's thread.
-         * @param event the event
-         */
-        @Override
-        public void actionPerformed(ActionEvent event) {
-            if (myTurn = true) {
-                output.format("hit\n");
-                output.flush();
-            }
-        }
-    }
-
-    private class standListener implements ActionListener {
-        /**
-         * Outputs a "stand\n" formatted message to server to make a "stand" on player's thread.
-         * @param event the event
-         */
-        @Override
-        public void actionPerformed(ActionEvent event) {
-            if (myTurn = true) {
-                output.format("stand\n");
-                output.flush();
-                myTurn = false;
-            }
-        }
     }
 }
